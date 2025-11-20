002d5fb30d1ad28bed57bdd77b26bb048cf1dfc5
Kusha Harchenko <wasdyner@mail.ru>
2025-11-20 23:30:37 +0500

    20.11.2025

--- jazz_project/jazz_app/urls.py
+++ jazz_project/jazz_app/urls.py
@@ -2,5 +2,5 @@ from jazz_app import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.online),
+    path('', views.start),
 ]
--- jazz_project/jazz_app/views.py
+++ jazz_project/jazz_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
-# Create your views here.
+def start(request):
+    context = {}
+    return render(request, 'header_footer.html', context)
